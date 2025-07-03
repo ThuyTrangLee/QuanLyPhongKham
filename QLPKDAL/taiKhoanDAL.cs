dc9440442822328f88d72e6f96fadf04ad84eef9
Thảo Trang <139950702+TrangThaoHuynh@users.noreply.github.com>
2025-07-03 09:17:11 +0700

    chuc nang login

---
+++ QLPKDAL/taiKhoanDAL.cs
@@ -0,0 +1,21 @@
+﻿using QLPKDTO;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLPKDAL
+{
+    public class taiKhoanDAL
+    {
+        private string connectionString;
+        public taiKhoanDAL()
+        {
+            connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+        }
+        public string ConnectionString { get => connectionString; set => connectionString = value;}
+    }
+}
